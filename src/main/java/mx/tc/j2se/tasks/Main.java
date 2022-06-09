c7c1f686d4ff3c527b2e80f19a2b001cd8a4fb41
Rodrigo <lobo.negroguionbajo3@gmail.com>
2022-06-09 18:03:36 -0500

    Primer commit: Práctica 0

---
+++ src/main/java/mx/tc/j2se/tasks/Main.java
@@ -0,0 +1,8 @@
+package mx.tc.j2se.tasks;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello Training Center!");
+	}
+}
+
---
+++ src/test/java/mx/tc/j2se/tasks/MainTest.java
@@ -0,0 +1,12 @@
+package mx.tc.j2se.tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+	@Test
+	public void mainTest() {
+		assertTrue(true);
+	}
+}
